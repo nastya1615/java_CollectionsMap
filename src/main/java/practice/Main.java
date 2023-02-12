814d99f5485ba676d247674f8ff110ff5aa6ddf6
Anastasya Ivanova <nastya1615325@gmail.com>
2023-02-12 16:24:21 +0400

    homework is done!

--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -1,7 +1,85 @@
 package practice;
 
+import javax.swing.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        PhoneBook phoneBook = new PhoneBook();
+
+        while (true) {
+
+            System.out.println("Введите номер, имя или команду");
+            Scanner scanner = new Scanner(System.in);
+            String[] words = scanner.nextLine().replaceAll("[-,+]", "").split("\\s");
+            String regexNumber = "[7-8][9]{1,3}[0-9]{7,9}";
+            String regexName = "[А-Я][а-я]{1,20}";
+
+            if (words.length == 1) {
+
+                if (words[0].matches(regexName)) {
+
+                    System.out.println("Введите номер телефона абонента " + words[0]);
+                    String namber = scanner.nextLine();
+
+                    if (namber.matches(regexNumber)){
+                        phoneBook.addContact(namber, words[0]);
+                    }
+
+                    else
+                        System.out.println("");
+
+
+
+                }
+
+                if (words[0].matches(regexNumber)) {
+
+                    System.out.println("Введите имя абонента для контакта " + words[0]);
+                    String name = scanner.nextLine();
+                    phoneBook.addContact(words[0], name);
+
+                }
+
+                if (words[0].equals("LIST")) {
+
+                    for (String contact : phoneBook.getAllContacts()) {
+                        System.out.println(contact);
+
+                    }
+
+                }
+
+
+            }
+
+            else {
+
+                if (words[0].equals("LISTC")) {
+
+                    System.out.println(words[1]);
+
+                    for (String contact : phoneBook.getContactByName(words[1])) {
+                        System.out.println("ghbdtn");
+                        System.out.println(contact);
+
+                    }
+
+                }
+
+                if (words[0].equals("LISTN")) {
+
+                    System.out.println(phoneBook.getContactByPhone(words[1]));
+
+
+
+                }
+            }
+
+
+        }
+
 
     }
 }
--- src/main/java/practice/PhoneBook.java
+++ src/main/java/practice/PhoneBook.java
@@ -1,40 +1,83 @@
 package practice;
 
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class PhoneBook {
 
+    TreeMap<String, String> contact = new TreeMap<>();
+    String regexNumber = "[7-8][9]{1,3}[0-9]{7,9}";
+    String regexName = "[А-Я][а-я]{1,20}";
+
+
     public void addContact(String phone, String name) {
-        // проверьте корректность формата имени и телефона
-        // (рекомедуется написать отдельные методы для проверки является строка именем/телефоном)
-        // если такой номер уже есть в списке, то перезаписать имя абонента
+
+        if (phone.matches(regexNumber) & name.matches(regexName)) {
+            contact.put(phone, name);
+        }
+
     }
+
 
     public String getContactByPhone(String phone) {
-        // формат одного контакта "Имя - Телефон"
-        // если контакт не найдены - вернуть пустую строку
-        return "";
+
+        return contact.get(phone) + " - " + phone;
+
+
     }
 
+
     public Set<String> getContactByName(String name) {
-        // формат одного контакта "Имя - Телефон"
-        // если контакт не найден - вернуть пустой TreeSet
-        return new TreeSet<>();
+
+        TreeSet<String> number = new TreeSet<>();
+
+        for (String key : contact.keySet()) {
+
+            if (contact.get(key).equals(name)) {
+
+                number.add(name + " - " + key);
+            }
+
+
+        }
+
+
+        return number;
     }
 
     public Set<String> getAllContacts() {
-        // формат одного контакта "Имя - Телефон"
-        // если контактов нет в телефонной книге - вернуть пустой TreeSet
-        return new TreeSet<>();
+
+
+        TreeSet<String> number = new TreeSet<>();
+
+        HashMap<String, String> newHasMap = new HashMap<>();
+
+        String num;
+
+        for (String key : contact.keySet()) {
+
+            num = key;
+
+            if (newHasMap.containsKey(contact.get(key))) {
+
+                num = newHasMap.get(contact.get(key)) + ", " + num;
+
+
+            }
+
+
+            newHasMap.put(contact.get(key), num);
+
+        }
+
+
+        for (String name : newHasMap.keySet()) {
+
+            number.add(name + " - " + newHasMap.get(name));
+
+        }
+
+        return number;
     }
 
-    // для обхода Map используйте получение пары ключ->значение Map.Entry<String,String>
-    // это поможет вам найти все ключи (key) по значению (value)
-    /*
-        for (Map.Entry<String, String> entry : map.entrySet()){
-            String key = entry.getKey(); // получения ключа
-            String value = entry.getValue(); // получения ключа
-        }
-    */
+
 }
